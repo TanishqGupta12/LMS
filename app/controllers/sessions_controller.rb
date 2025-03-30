aae73c9102c17038d68b74761fa1be3c87b403bd
Tanishq Gupta <tanishq@hipster-dev.com>
2025-03-30 09:04:21 +0530

    session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < Devise::SessionsController
   # GET /resource/sign_in
   def new
-    redirect_to login_url(event_id: params[:event_id]) and return
+    redirect_to main_app.login_url(event_id: session[:event_id]) and return
   end
 
 end
