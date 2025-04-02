b3ba342d07c44630751b3eba42708f5d52737587
Tanishq Gupta <tanishq@hipster-dev.com>
2025-04-02 22:42:44 +0530

    gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  acts_as_favoritable
+
   acts_as_voter
+
+  acts_as_favoritable
+  acts_as_favoritor
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
