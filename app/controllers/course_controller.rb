fa10d190466b081b5313f3e06f95e475859288e4
Tanishq Gupta <tanishq@hipster-dev.com>
2025-04-05 10:42:54 +0530

    fix

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,11 +1,10 @@
 class CourseController < ApplicationController
   
  def index
-  # Correct Query Usage "category"=>"Web developer", "course"=>"Angular"
-  # @course = Course.includes(:category)
-  # Paginate results
-   # Show 10 records per page
-
   @courses = Course.includes(:ticket).where(event_id: @event.id).category_search(params[:category], params[:course]) .page(params[:page]).per(5)
+ end
+
+ def show
+   
  end
 end
