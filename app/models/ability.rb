d4ec56acc20e8c02ce485d31f509e4408800428e
Tanishq Gupta <tanishq@hipster-dev.com>
2025-04-07 10:48:06 +0530

    fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -20,8 +20,9 @@ class Ability
       can :manage, :all
       cannot :manage, Role # Restrict role management for admins
     elsif user.teacher?
-      can :read, Category
       can :read, User
+      can :read, Category , event_id: user.current_event_id.to_i
+      
       can :manage ,Blog ,user_id: user.id , event_id: user.current_event_id.to_i
       can :manage, Ticket ,user_id: user.id , event_id: user.current_event_id.to_i
       # Allow teacher to manage courses where teacher_id and event_id match
