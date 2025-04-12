2a163db451ddbfbc395e8714f91b0965dbc11bc5
Tanishq Gupta <tanishq@hipster-dev.com>
2025-04-12 14:48:35 +0530

    layout

--- app/controllers/user_notes_controller.rb
+++ app/controllers/user_notes_controller.rb
@@ -1,6 +1,18 @@
 class UserNotesController < ApplicationController
 
   def create
-    debugger
+    @user_note = UserNote.new(user_note_params)
+    if @user_note.save
+      # flash[:notice] = "Note saved successfully"
+      redirect_to fallback_location: request.referer , notice: "Successfully saved!"
+    else
+      redirect_to fallback_location: request.referer , alert: "Something went wrong."
+    end
+  end
+
+  private
+
+  def user_note_params
+    params.permit(:user_id, :event_id, :course_id, :subject, :description)
   end
 end
