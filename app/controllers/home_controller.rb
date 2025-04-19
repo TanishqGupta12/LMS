24aeb0a67d74a4340e56f86d07f8dd9425d6f0d5
Tanishq Gupta <tanishq@hipster-dev.com>
2025-04-19 12:08:42 +0530

    sign

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,11 +17,7 @@ class HomeController < ApplicationController
   end
 
   def sign_up
-    # @form = Form
-    # .includes(:form_sections , )
-    # .includes(form_section_fields: :form_field_choices) 
-    # .find_by(event_id: params[:event_id])                      
-    # 
+    
     @form = Form.includes(form_sections: { form_section_fields: :form_field_choices }).find_by(event_id: params[:event_id])
 
   end
