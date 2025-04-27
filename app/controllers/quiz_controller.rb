e776da0368a73e48323cbc33fb870044d6120701
Tanishq Gupta <tanishq@hipster-dev.com>
2025-04-27 17:41:35 +0530

    file

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,6 +1,6 @@
 class QuizController < ApplicationController
 
   def index
-    
+    @questions = QuizQuestion.includes(:quiz_question_options).where(lesson_id: params[:lesson]).order(:sequence)
   end
 end
--- app/models/quiz_question.rb
+++ app/models/quiz_question.rb
@@ -2,5 +2,7 @@ class QuizQuestion < ApplicationRecord
 
   belongs_to :quiz_topic , optional: true
   belongs_to :lesson , optional: true
+
+  has_many :quiz_question_options
   enum question_type: { "Multiple Choice Multiple option": "Multiple Choice Multiple Option" , "Multiple Choice Single Option": "Multiple choice Single option" , "True False": "True False" , "Short Answer": "Short Answer"}
 end
